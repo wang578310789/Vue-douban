1ca16975e61810b444c55f93851893a9e61d1018
wy <578310789@qq.com>
2018-12-18 16:30:41 +0800

    x

---
+++ precache-manifest.657dfd536dd8a3871a026ba6ece45450.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2894918c2d3fad3f8337",
+    "url": "/js/chunk-vendors.8e475858.js"
+  },
+  {
+    "revision": "57053cdc8825beed88dd",
+    "url": "/js/chunk-02e074da.49077a6e.js"
+  },
+  {
+    "revision": "d599542cf45b2a4e9bbf",
+    "url": "/js/app.09b87427.js"
+  },
+  {
+    "revision": "01bb02221c0d164f3163940e95bf2525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57053cdc8825beed88dd",
+    "url": "/css/chunk-02e074da.72d56332.css"
+  },
+  {
+    "revision": "d599542cf45b2a4e9bbf",
+    "url": "/css/app.edec95d0.css"
+  }
+];
